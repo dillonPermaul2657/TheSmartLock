14e0acfbc29438d089530d33b53d1ed0adbf0597
KamaljitMahal7647 <n01377647@humbermail.ca>
2021-09-24 19:28:32 -0400

    Commented all group members names as well as their student number and section.

--- TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/MainActivity.java
+++ TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/MainActivity.java
@@ -12,7 +12,11 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
-//Kamaljit Mahal n01377647
+//Kamaljit Mahal N01377647 Section B
+//Dillon Permaul N01372657 Section B
+//Janpreet Singh N01361405 Section B
+//Meet Gajjar N01391319 Section B
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
