03f7316304f681db992601d3bde1c5aae611dc61
MeetGajjar1319 <n01391319@humbermail.ca>
2021-09-26 14:47:49 -0400

    Added team members name and student id in all java class

--- TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/home/HomeViewModel.java
+++ TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/home/HomeViewModel.java
@@ -1,3 +1,9 @@
+//Kamaljit Mahal N01377647 Section B
+//Dillon Permaul N01372657 Section B
+//Janpreet Singh N01361405 Section B
+//Meet Gajjar N01391319 Section B
+
+
 package ca.thedjkm.it.smartlock.ui.home;
 
 import androidx.lifecycle.LiveData;
