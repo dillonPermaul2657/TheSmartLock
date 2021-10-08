3cc6979d6acd3382cd4b92b194a4b39a884f2442
dillonPermaul2657 <n01372657@humbermail.ca>
2021-10-08 11:28:49 -0400

    Added names and student number as comments in SplashScreenActivity.java

--- TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/SplashScreenActivity.java
+++ TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/SplashScreenActivity.java
@@ -1,14 +1,21 @@
 package ca.thedjkm.it.smartlock;
+//Kamaljit Mahal N01377647 Section B
+//Dillon Permaul N01372657 Section B
+//Janpreet Singh N01361405 Section B
+//Meet Gajjar N01391319 Section B
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
+        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        finish();
     }
 }
