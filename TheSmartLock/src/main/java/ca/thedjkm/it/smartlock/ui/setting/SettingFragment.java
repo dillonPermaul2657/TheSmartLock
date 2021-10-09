c6f72372e395786d351fc67c7a40e82e67fb376b
MeetGajjar1319 <n01391319@humbermail.ca>
2021-10-09 00:26:05 -0400

    Added setting screen to the project

---
+++ TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/setting/SettingFragment.java
@@ -0,0 +1,25 @@
+package ca.thedjkm.it.smartlock.ui.setting;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import ca.thedjkm.it.smartlock.R;
+
+public class SettingFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       // return super.onCreateView(inflater, container, savedInstanceState);
+
+        View root = inflater.inflate(R.layout.fragment_setting,container,false);
+
+        return root;
+    }
+}
