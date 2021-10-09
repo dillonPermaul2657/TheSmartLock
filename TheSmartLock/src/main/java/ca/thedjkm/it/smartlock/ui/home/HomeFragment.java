cec6451bff6cc654b333780eb84d936d1f26f605
KamaljitMahal7647 <n01377647@humbermail.ca>
2021-10-09 16:37:03 -0400

    Created a textview that displays the current time and date (Real time)

--- TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/home/HomeFragment.java
+++ TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/home/HomeFragment.java
@@ -17,24 +17,30 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import org.w3c.dom.Text;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import ca.thedjkm.it.smartlock.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    TextView textViewDate;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+
+        textViewDate = (TextView) view.findViewById(R.id.text_view_date);
+
+        Calendar calendar = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss a");
+        String dateTime = simpleDateFormat.format(calendar.getTime());
+        textViewDate.setText(dateTime);
+        return view;
     }
 }
+
