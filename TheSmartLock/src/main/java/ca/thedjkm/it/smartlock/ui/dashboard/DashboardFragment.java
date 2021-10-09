614538dc72908145c4ad5a4fbab26922c2bdc9f4
dillonPermaul2657 <n01372657@humbermail.ca>
2021-10-09 13:27:26 -0400

    Added two image buttons to lock and unlock with a toast message

--- TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/dashboard/DashboardFragment.java
+++ TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/dashboard/DashboardFragment.java
@@ -6,14 +6,19 @@
 
 package ca.thedjkm.it.smartlock.ui.dashboard;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -22,20 +27,46 @@ import ca.thedjkm.it.smartlock.R;
 
 public class DashboardFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                new ViewModelProvider(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+    ImageButton lockButton1;
+    ImageButton unlockButton2;
+    RelativeLayout relativeLayout;
+
+
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_dashboard,container,false);
+
+        lockButton1 = (ImageButton) view.findViewById(R.id.imageButton1);
+        unlockButton2 = (ImageButton) view.findViewById(R.id.imageButton2);
+        relativeLayout = (RelativeLayout) view.findViewById(R.id.rlVar1);
+
+        lockButton1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onClick(View v) {
+                relativeLayout.setBackgroundResource(R.color.red);
+                Toast.makeText(getActivity(),"The Door is Locked!",Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        unlockButton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                relativeLayout.setBackgroundResource(R.color.green);
+                Toast.makeText(getActivity(),"The Door is Unlocked!",Toast.LENGTH_SHORT).show();
+
             }
         });
-        return root;
+
+        return view;
+
     }
+
+
+
+
 }
