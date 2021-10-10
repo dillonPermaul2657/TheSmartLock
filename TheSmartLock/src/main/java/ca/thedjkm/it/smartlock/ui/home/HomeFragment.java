466e5c091652456a34da2faf9f5761c9a8fe2661
KamaljitMahal7647 <n01377647@humbermail.ca>
2021-10-10 16:13:26 -0400

    Added code where if the user clicks the request permission button it asks the user to allow or deny permissions if user allows the permissions, it is allowed forever until changed in app settings.

--- TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/home/HomeFragment.java
+++ TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/home/HomeFragment.java
@@ -6,16 +6,21 @@
 package ca.thedjkm.it.smartlock.ui.home;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
@@ -27,33 +32,90 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import ca.thedjkm.it.smartlock.MainActivity;
 import ca.thedjkm.it.smartlock.R;
 
 public class HomeFragment extends Fragment {
 
     TextView textViewDate;
     TextView textView;
+    private int STORAGE_PERMISSION_CODE = 1;
+    Button buttonRequest;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        buttonRequest = (Button) view.findViewById(R.id.button);
+        buttonRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(getActivity(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getActivity(), "You have already granted this permission!",
+                            Toast.LENGTH_SHORT).show();
+                } else {
+                    requestStoragePermission();
+                }
+            }
+
+            private void requestStoragePermission() {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    new AlertDialog.Builder(getActivity())
+                            .setTitle("Permission needed")
+                            .setMessage("This permission is needed because of this and that")
+                            .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    ActivityCompat.requestPermissions(getActivity(),
+                                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                                }
+                            })
+                            .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            })
+                            .create().show();
+                } else {
+                    ActivityCompat.requestPermissions(getActivity(),
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+                }
+            }
 
 
 
-        //code for animated text, does not work for some reason at the moment
-        textView = (TextView) view.findViewById(R.id.marquee);
-        textView.setEllipsize(TextUtils.TruncateAt.MARQUEE);
-        textView.setSelected(true);
+            private void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+                if (requestCode == STORAGE_PERMISSION_CODE) {
+                    if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(getActivity(), "Permission GRANTED", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getActivity(), "Permission DENIED", Toast.LENGTH_SHORT).show();
 
-        //code to show the current date and time
-        textViewDate = (TextView) view.findViewById(R.id.text_view_date);
 
-        Calendar calendar = Calendar.getInstance();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss a");
-        String dateTime = simpleDateFormat.format(calendar.getTime());
-        textViewDate.setText(dateTime);
+                        //code for animated text, does not work for some reason at the moment
+                        textView = (TextView) view.findViewById(R.id.marquee);
+                        textView.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+                        textView.setSelected(true);
+
+                        //code to show the current date and time
+                        textViewDate = (TextView) view.findViewById(R.id.text_view_date);
+
+                        Calendar calendar = Calendar.getInstance();
+                        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss a");
+                        String dateTime = simpleDateFormat.format(calendar.getTime());
+                        textViewDate.setText(dateTime);
+
+                    }
+                }
+            }
+        });
         return view;
     }
 }
+
+
 
