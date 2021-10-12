9aecefd7153ff094b848927b6805049b49828e0e
dillonPermaul2657 <n01372657@humbermail.ca>
2021-10-12 13:05:45 -0400

    Translated strings to french in fr/strings.xml and added comments of member names and student number to remaining java classes

--- TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/Preference.java
+++ TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/Preference.java
@@ -2,7 +2,10 @@ package ca.thedjkm.it.smartlock;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
-
+// Kamaljit Mahal N01377647 Section B
+// Dillon Permaul N01372657 Section B
+// Janpreet Singh N01361405 Section B
+// Meet Gajjar N01391319 Section B
 
 
 public class Preference extends PreferenceActivity {
--- TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/SplashScreenActivity.java
+++ TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/SplashScreenActivity.java
@@ -7,6 +7,10 @@ import android.os.Bundle;
 import android.view.View;
 
 import gr.net.maroulis.library.EasySplashScreen;
+// Kamaljit Mahal N01377647 Section B
+// Dillon Permaul N01372657 Section B
+// Janpreet Singh N01361405 Section B
+// Meet Gajjar N01391319 Section B
 
 public class SplashScreenActivity extends AppCompatActivity {
 
