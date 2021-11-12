fc1f59df7a9ee09950989d7e3eaf0342bb2f0e0f
MeetGajjar1319 <n01391319@humbermail.ca>
2021-11-11 22:08:13 -0500

    update  Setting screen (add functionality of portrait mode)

--- TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/Preference.java
+++ TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/Preference.java
@@ -17,35 +17,35 @@ import android.preference.PreferenceManager;
 public class Preference extends PreferenceActivity {
 
     @Override
-    protected void onCreate( Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.setting_screen);
         Load_setting(); //error
 
     }
 
-    private void Load_setting(){
+    private void Load_setting() {
 
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
 
-        boolean chk_night = sp.getBoolean("NIGHT",false);
-        if(chk_night){
+        boolean chk_night = sp.getBoolean("NIGHT", false);
+        if (chk_night) {
             getListView().setBackgroundColor(Color.parseColor("#222222"));
         } else {
             getListView().setBackgroundColor(Color.parseColor("#ffffff"));
         }
 
 
-        CheckBoxPreference chk_night_instant = (CheckBoxPreference)findPreference("NIGHT");
+        CheckBoxPreference chk_night_instant = (CheckBoxPreference) findPreference("NIGHT");
         chk_night_instant.setOnPreferenceChangeListener(new android.preference.Preference.OnPreferenceChangeListener() {
             @Override
             public boolean onPreferenceChange(android.preference.Preference setting_screen, Object obj) {
 
-                boolean yes = (boolean)obj;
+                boolean yes = (boolean) obj;
 
-                if (yes){
+                if (yes) {
                     getListView().setBackgroundColor(Color.parseColor("#222222"));
-                }else{
+                } else {
                     getListView().setBackgroundColor(Color.parseColor("#ffffff"));
                 }
 
@@ -53,12 +53,49 @@ public class Preference extends PreferenceActivity {
             }
         });
 
+        ListPreference LP = (ListPreference) findPreference("ORIENTATION");
 
-        };
+        String orien = sp.getString("ORIENTATION", "false");
+        if ("1".equals(orien)) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_BEHIND);
+            LP.setSummary(LP.getEntry());
+        } else if ("2".equals(orien)) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            LP.setSummary(LP.getEntry());
+        } else if ("3".equals(orien)) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            LP.setSummary(LP.getEntry());
+        }
+
+        LP.setOnPreferenceChangeListener(new android.preference.Preference.OnPreferenceChangeListener() {
+            @Override
+            public boolean onPreferenceChange(android.preference.Preference setting_screen, Object obj) {
+                String items = (String) obj;
+                if (setting_screen.getKey().equals("ORIENTATION")) {
+                    switch (items) {
+                        case "1":
+                            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_BEHIND);
+                            break;
+                        case "2":
+                            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                            break;
+                        case "3":
+                            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                            break;
+                    }
+
+                    ListPreference LPP = (ListPreference) setting_screen;
+                    LPP.setSummary(LPP.getEntries()[LPP.findIndexOfValue(items)]);
+
+                }
+
+                return true;
+            }
+        });
 // 15:00 mins
 
 
-
+    }
 
     @Override
     protected void onResume() {
