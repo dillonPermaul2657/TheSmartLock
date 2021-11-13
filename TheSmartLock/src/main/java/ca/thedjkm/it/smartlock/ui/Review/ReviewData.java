2f23c8db9e0201829a513c11db231376c1cf25cd
dillonPermaul2657 <n01372657@humbermail.ca>
2021-11-12 19:40:32 -0500

    Added team members names, student id and section as a comment to ReviewData.java

--- TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/Review/ReviewData.java
+++ TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/Review/ReviewData.java
@@ -1,4 +1,8 @@
 package ca.thedjkm.it.smartlock.ui.Review;
+//Kamaljit Mahal N01377647 Section B
+//Dillon Permaul N01372657 Section B
+//Janpreet Singh N01361405 Section B
+//Meet Gajjar N01391319 Section B
 
 public class ReviewData {
 
