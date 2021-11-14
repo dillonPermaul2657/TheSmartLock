2b420bffb5333d9e9d259f426228aeffc49bec94
MeetGajjar1319 <n01391319@humbermail.ca>
2021-11-14 13:17:23 -0500

    Update MotionFragment.java file

--- TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/setting/MotionFragment.java
+++ TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/setting/MotionFragment.java
@@ -4,10 +4,14 @@
 // Meet Gajjar N01391319 Section B
 package ca.thedjkm.it.smartlock.ui.setting;
 
+import static android.widget.Toast.*;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -23,9 +27,20 @@ public class MotionFragment extends Fragment {
        // return super.onCreateView(inflater, container, savedInstanceState);
 
         View root = inflater.inflate(R.layout.fragment_motion,container,false);
+        Button btn=(Button) root.findViewById(R.id.ON);
+
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Motion Sensor is on", LENGTH_SHORT).show();
+            }
+        });
+
 
         return root;
     }
+
 
 
 
