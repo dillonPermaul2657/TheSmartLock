f1c884b523785b459461be76c7ee6c8a88462c79
dillonPermaul2657 <n01372657@humbermail.ca>
2021-11-30 15:18:03 -0500

    Added members names, student numbers and section on top of java file as a comment

--- TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/Review/ProgressBarReview.java
+++ TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/Review/ProgressBarReview.java
@@ -1,3 +1,7 @@
+//Kamaljit Mahal N01377647 Section B
+//Dillon Permaul N01372657 Section B
+//Janpreet Singh N01361405 Section B
+//Meet Gajjar N01391319 Section B
 package ca.thedjkm.it.smartlock.ui.Review;
 
 import android.app.Activity;
@@ -7,8 +11,8 @@ import android.view.LayoutInflater;
 import ca.thedjkm.it.smartlock.R;
 
 public class ProgressBarReview {
-    Activity activity;
-    AlertDialog dialog;
+   private Activity activity;
+   private AlertDialog dialog;
 
     ProgressBarReview(Activity myActivity){
         activity = myActivity;
