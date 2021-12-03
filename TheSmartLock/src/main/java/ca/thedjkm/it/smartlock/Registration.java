cd69eb641e7d965ff1d8d89286aa517e7c794c54
JanpreetSinghN01361405 <n01361405@humbermail.ca>
2021-12-03 14:39:25 -0500

    Added java code placeholder to validate registration screen. added toast on register button click

--- TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/Registration.java
+++ TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/Registration.java
@@ -3,12 +3,39 @@ package ca.thedjkm.it.smartlock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class Registration extends AppCompatActivity {
+
+    EditText name;
+    EditText email;
+    EditText pass;
+    Button register;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
-    }
-}
+
+
+        name = findViewById(R.id.txtName);
+        email = findViewById(R.id.txtEmail);
+        pass = findViewById(R.id.txtPwd);
+        register = findViewById(R.id.btnrgstr);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "You have been registered", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
+
+        }};
