5f0865dd3f34ffd9b1bdc8c3ee26e698c1c130f8
dillonPermaul2657 <n01372657@humbermail.ca>
2021-12-03 12:12:17 -0500

    Add a button to NotificationsFragment to implement a notification

--- TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/notifications/NotificationsFragment.java
+++ TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/notifications/NotificationsFragment.java
@@ -4,27 +4,62 @@
 // Meet Gajjar N01391319 Section B
 package ca.thedjkm.it.smartlock.ui.notifications;
 
+import static android.content.Context.NOTIFICATION_SERVICE;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import android.content.Context;
 
 import ca.thedjkm.it.smartlock.R;
 
 public class NotificationsFragment extends Fragment {
 
+    Button button;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // return super.onCreateView(inflater, container, savedInstanceState);
 
-        View root = inflater.inflate(R.layout.fragment_notifications,container,false);
+        View view = inflater.inflate(R.layout.fragment_notifications,container,false);
 
-        return root;
+        button = (Button) view.findViewById(R.id.NotiButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                NotificationCompat.Builder mbuilder =
+                        new NotificationCompat.Builder(getContext().getApplicationContext())
+                        .setSmallIcon(R.drawable.ic_lock_foreground)
+                        .setContentTitle(getString(R.string.notif))
+                        .setContentText(getString(R.string.NotiMessage));
+
+               NotificationManager notificationManager = (NotificationManager)
+                        getActivity().getSystemService(getActivity().NOTIFICATION_SERVICE);
+                notificationManager.notify(0,mbuilder.build());
+
+            }
+        });
+
+        return view;
     }
+
+
+
+
 }
