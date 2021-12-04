67dbfd686026c883117fa136eda29bc9753babd0
JanpreetSinghN01361405 <n01361405@humbermail.ca>
2021-12-04 16:37:08 -0500

    Added Validation for registration email password name. fixed some overlapping error

--- TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/registration/Registration.java
+++ TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/registration/Registration.java
@@ -8,6 +8,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -27,7 +28,7 @@ public class Registration extends AppCompatActivity {
     EditText email;
     EditText pass;
     Button register;
-
+    boolean isNameValid, isEmailValid, isPasswordValid;
 
 
     @Override
@@ -47,35 +48,75 @@ public class Registration extends AppCompatActivity {
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                SetValidation();
+                if (isNameValid && isEmailValid && isPasswordValid) {
+                    Toast.makeText(getApplicationContext(), "Successfully Registered", Toast.LENGTH_SHORT).show();
 
-                progressBarReview.loadingProgressBar();
-                Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        progressBarReview.dismissProgressBar();
-                    }
-                },2000);
-
-
-                RegisterData reg = new RegisterData(name.getText().toString(),email.getText().toString(),
-                        pass.getText().toString());
-                doa.add(reg).addOnSuccessListener(suc ->
-                {
-                    Toast toast = Toast.makeText(getApplicationContext(), "You have been registered", Toast.LENGTH_LONG);
-                    toast.show();
-
-                }).addOnFailureListener(er ->
-                        {
-                            Toast toast = Toast.makeText(getApplicationContext(), "Please try again", Toast.LENGTH_LONG);
-                            toast.show();
-                           // Toast.makeText(getActivity(),""+er.getMessage(),Toast.LENGTH_SHORT).show();
+                    progressBarReview.loadingProgressBar();
+                    Handler handler = new Handler();
+                    handler.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBarReview.dismissProgressBar();
                         }
-                );
+                    }, 2000);
 
 
+                    RegisterData reg = new RegisterData(name.getText().toString(), email.getText().toString(),
+                            pass.getText().toString());
+                    doa.add(reg).addOnSuccessListener(suc ->
+                    {
+                        Toast toast = Toast.makeText(getApplicationContext(), "You have been registered", Toast.LENGTH_LONG);
+                        toast.show();
+
+                    }).addOnFailureListener(er ->
+                            {
+                                Toast toast = Toast.makeText(getApplicationContext(), "Please try again", Toast.LENGTH_LONG);
+                                toast.show();
+                                // Toast.makeText(getActivity(),""+er.getMessage(),Toast.LENGTH_SHORT).show();
+                            }
+                    );
+
+                }
             }
         });
 
 
-        }};
+    }
+
+    public void SetValidation() {
+        // Check for a valid name.
+        if (name.getText().toString().isEmpty()) {
+            name.setError(getResources().getString(R.string.name_error));
+            isNameValid = false;
+        } else {
+            isNameValid = true;
+        }
+
+        // Check for a valid email address.
+        if (email.getText().toString().isEmpty()) {
+            email.setError(getResources().getString(R.string.email_error));
+            isEmailValid = false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email.getText().toString()).matches()) {
+            email.setError(getResources().getString(R.string.error_invalid_email));
+            isEmailValid = false;
+        } else {
+            isEmailValid = true;
+        }
+
+        // Check for a valid password.
+        if (pass.getText().toString().isEmpty()) {
+            pass.setError(getResources().getString(R.string.password_error));
+            isPasswordValid = false;
+        } else if (pass.getText().length() < 6) {
+            pass.setError(getResources().getString(R.string.error_invalid_password));
+            isPasswordValid = false;
+        } else {
+            isPasswordValid = true;
+        }
+
+        if (isNameValid && isEmailValid && isPasswordValid) {
+            Toast.makeText(getApplicationContext(), "Successfully Registered", Toast.LENGTH_SHORT).show();
+        }
+    }
+};
