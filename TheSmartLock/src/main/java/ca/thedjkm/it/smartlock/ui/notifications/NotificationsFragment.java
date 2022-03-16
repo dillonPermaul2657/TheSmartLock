e53b944c1dda65b85daef6b90aab75f90821bda3
dillonPermaul2657 <n01372657@humbermail.ca>
2022-03-16 11:20:22 -0400

    Added function to get RFID data from database

--- TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/notifications/NotificationsFragment.java
+++ TheSmartLock/src/main/java/ca/thedjkm/it/smartlock/ui/notifications/NotificationsFragment.java
@@ -25,39 +25,73 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import android.content.Context;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.muddzdev.styleabletoastlibrary.StyleableToast;
+
+import ca.thedjkm.it.smartlock.MainActivity;
 import ca.thedjkm.it.smartlock.R;
 
 public class NotificationsFragment extends Fragment {
 
     Button button;
 
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
+    private TextView retrieveTV;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         // return super.onCreateView(inflater, container, savedInstanceState);
 
-        View view = inflater.inflate(R.layout.fragment_notifications,container,false);
+        View view = inflater.inflate(R.layout.fragment_notifications, container, false);
 
         button = (Button) view.findViewById(R.id.NotiButton);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference("RFID");
+        retrieveTV = view.findViewById(R.id.idTVRetrieveData);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        getdata();
 
-                NotificationCompat.Builder mbuilder =
-                        new NotificationCompat.Builder(getContext().getApplicationContext())
-                        .setSmallIcon(R.drawable.ic_lock_foreground)
-                        .setContentTitle(getString(R.string.notif))
-                        .setContentText(getString(R.string.NotiMessage));
 
-               NotificationManager notificationManager = (NotificationManager)
-                        getActivity().getSystemService(getActivity().NOTIFICATION_SERVICE);
-                notificationManager.notify(0,mbuilder.build());
 
-            }
-        });
+
+
 
         return view;
     }
+
+    private void getdata() {
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // this method is call to get the realtime
+                // updates in the data.
+                // this method is called when the data is
+                // changed in our Firebase console.
+                // below line is for getting the data from
+                // snapshot of our database.
+                String value = snapshot.getValue(String.class);
+
+                // after getting the value we are setting
+                // our value to our text view in below line.
+                retrieveTV.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                StyleableToast.makeText(getActivity(), getString(R.string.dr_lock),Toast.LENGTH_SHORT ,R.style.door_lock).show();
+            }
+
+
+
+    });
+}
 
 
 
